bafbc11501f2b0c3ceb42fb2c856457055a82de2
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-12 15:04:39 -0800

    product model

--- models/Product.js
+++ models/Product.js
@@ -11,27 +11,44 @@ Product.init(
   {
     //idcolumn
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
 
     //product_name column
     product_name: {
-
+      type: DataTypes.STRING,
+      allowNull: false
     },
   
     //price column
     price: {
-
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
 
     //stock column
     stock: {
-
+      tyoe: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
     },
 
     //category_id column
     category_d: {
-      
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
