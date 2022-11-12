93c9992e16f6a631a1613cd92fc45055d0437681
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-12 14:16:22 -0800

    Category model columns

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,19 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    //Define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
+
+    //Define category_name column
+    category_name :{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
